5c9e8bb122e456a77b50746862a4b2fbb7f25620
Dilnaz Zhumabaikyzy <dilnazzh200303@gmail.com>
2023-11-03 15:58:36 +0600

    initial commit

---
+++ src/main/java/com/example/nest_back/NestBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.nest_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NestBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NestBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/nest_back/NestBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nest_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NestBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
